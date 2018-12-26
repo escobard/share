e5c9f5691029af5f9f0074c1188ba276e6cb5fc8
Daniel Escobar <escobard90@gmail.com>
2018-12-26 14:29:53 -0700

    uml-activity - activity: updated UML activity diagram w/ improved lottery logic, updated share_prototype.sol

--- architecture/Contracts/share_prototype.sol
+++ architecture/Contracts/share_prototype.sol
@@ -8,6 +8,32 @@
 
     // Variable: donor - donor public address
 
+    // Variable: amount
+
     // Mapping: donationSku
 
-    // Mapping: lotterySku
+    // State: Donated, enteredLottery, Fulfilled, amountRemaining
+
+    // Struct: Donation { donationSku, amount, state, owner, lottery, charity, donor  }
+
+    // Modifier: 'isOwner' confirms that the caller is the owner
+
+    // Modifier: 'isDonor' confirms that the caller is the donor
+
+    // Modifier: 'isDonated' confirms that the donation has been donated (state)
+
+    // Modifier: 'hasEntered' confirms that the donation has been entered to the lottery (state)
+
+    // Modifier: 'isFulfilled' confirms that the donation lifecycle has ended
+
+    // Constructor Function: Set 'owner' to the address that instantiated the contract
+
+    // Function: 'dispatchOwner' dispatches 1% of the amount to the owner address, updates state
+
+    // Function: 'dispatchCharity' dispatches 95% of the amount to the charity address, updates state
+
+    // Function: 'dispatchLottery' dispatches 4% of the amount to lottery, updates state
+
+    // Function: 'handleFunds' handles all the dispatch functionality
+
+    // Function: 'enterLottery'
