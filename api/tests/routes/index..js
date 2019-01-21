94f65df0e82274532df84cc241243766ca7ae6b1
Daniel Escobar <escobard90@gmail.com>
2019-01-20 23:04:29 -0700

    api-base - refact: updated readability of testing index file

--- api/tests/routes/index..js
+++ api/tests/routes/index..js
@@ -1,32 +1,41 @@
 const request = require("supertest");
-describe("Testing all routes", function() {
+
+describe("Testing all routes", () =>{
+
   let server;
+
   beforeEach(() => {
     server = require("../../index");
   });
+
   afterEach(() => {
     server.close();
   });
+
   it("responds to /", done => {
     request(server)
       .get("/")
       .expect(200, done);
   });
+
   it("responds to /greetings post requests", done => {
         request(server)
             .post("/greetings")
             .send({name: 'John'})
             .expect(200, done)
     });
-    it("rejects /greetings post requests with missing name property", done => {
+
+  it("rejects /greetings post requests with missing name property", done => {
         request(server)
             .post("/greetings")
             .send({})
             .expect(400, done)
-    });
+  });
+
   it("404 everything else", done => {
     request(server)
       .get("/foo/bar")
       .expect(404, done);
   });
+
 });
