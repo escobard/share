864e24974eb71e2e667bad232bc90c96c745fcf7
Daniel Escobar <escobard90@gmail.com>
2019-01-20 22:31:36 -0700

    api-base - refact: updated greetings route with es6, tested and working

--- api/routes/greetings.js
+++ api/routes/greetings.js
@@ -1,9 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 // the route here is replaced by the route passed within ./index.js
-router.post('/', (req, res) => {
-    console.log('request: ', req.body)
-    res.send("Greetings ");
+router.post("/", (req, res) => {
+  console.log("request: ", req.name);
+  !req.body.name
+    ? res.status(400).json("Bad request, body of request missing name property")
+    : res.status(200).json("Greetings");
 });
 
 module.exports = router;
