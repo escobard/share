e570d0ea67ffe71a70e32f064f7c65c6f51f6f58
Daniel Escobar <escobard90@gmail.com>
2019-02-01 15:51:48 -0700

    ethereum-share-contract - feat: added base variables and mappings

--- ethereum/contracts/Share.sol
+++ ethereum/contracts/Share.sol
@@ -4,5 +4,25 @@ pragma solidity ^0.4.23;
 
 contract Share {
 
+    address Owner;
+    address Lottery;
+    address Charity;
+
+
+
+    /// @notice Contains the mapping for the lottery entrees
+    /// @dev
+    /// @param donor address, will be expanded for v2.0
+
+    mapping(address => Entrees) public lotteryEntrees;
+
+    /// @notice Contains the mapping for donation data
+    /// @dev key of structure is the transactionHash, in v2.0 a donationId will be introduced
+    /// @param Donation structure, contains donation metadata
+
+    mapping(address => Donation) public Donations;
+
+
+
 }
 
