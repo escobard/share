2569948a8c21b7e4662ac9d7f07132c218cd51d2
Daniel Escobar <escobard90@gmail.com>
2019-02-26 20:21:45 -0700

    ui-reusable-form - feat: added renderFields to generate scalable form fields

--- ui/src/components/ShareForm/index.js
+++ ui/src/components/ShareForm/index.js
@@ -26,7 +26,6 @@ class ShareForm extends Component {
           this.setState({hasFields: true});
 
           fields.map((field, index) =>{
-              console.log('INDEX', index);
               this.inputState(field, index);
           })
       }
@@ -55,29 +54,29 @@ class ShareForm extends Component {
       })
   }
 
-/*
   renderFields = (fields) => {
-      if (fields){
-          fields.map((field, index) =>{
-              let { label, placeholder } = field;
+          return fields.map((field, index) =>{
+              return(
+                  <Form.Field
+                      key={index}
+                      control={Input}
+                      label="First name"
+                      placeholder="First name"
+                  />
+                  )
+
           })
-      }
-      else{
-          return <p>No form fields passed to component</p>
-      }
+
     };
-*/
+
   render() {
-    const { value, hasFields } = this.state;
+    const { hasFields } = this.state;
+    let { fields } = this.props;
     return (
         <Fragment>
             {hasFields ? <Form>
                 <Form.Group widths="equal">
-                    <Form.Field
-                        control={Input}
-                        label="First name"
-                        placeholder="First name"
-                    />
+                    {this.renderFields(fields)}
                     <Form.Field
                         control={Input}
                         label="Last name"
@@ -90,39 +89,6 @@ class ShareForm extends Component {
                         placeholder="Gender"
                     />
                 </Form.Group>
-                <Form.Group inline>
-                    <label>Quantity</label>
-                    <Form.Field
-                        control={Radio}
-                        label="One"
-                        value="1"
-                        checked={value === "1"}
-                        onChange={this.handleChange}
-                    />
-                    <Form.Field
-                        control={Radio}
-                        label="Two"
-                        value="2"
-                        checked={value === "2"}
-                        onChange={this.handleChange}
-                    />
-                    <Form.Field
-                        control={Radio}
-                        label="Three"
-                        value="3"
-                        checked={value === "3"}
-                        onChange={this.handleChange}
-                    />
-                </Form.Group>
-                <Form.Field
-                    control={TextArea}
-                    label="About"
-                    placeholder="Tell us more about you..."
-                />
-                <Form.Field
-                    control={Checkbox}
-                    label="I agree to the Terms and Conditions"
-                />
                 <Form.Field control={Button}>Submit</Form.Field>
             </Form> : <p>Form has no input props!</p>}
       </Fragment>
