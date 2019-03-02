dcc0b22bb59f6c2f2f5f5d054b7d9b77696c2278
Daniel Escobar <escobard90@gmail.com>
2019-03-02 15:01:34 -0700

    ui-make-donation-form - feat: added several potential TODO's, form logic will be centralized within the ShareForm container at least for share 1.0

--- ui/src/components/ShareForm/index.js
+++ ui/src/components/ShareForm/index.js
@@ -1,116 +1,124 @@
 import React, { Component, Fragment } from "react";
 import axios from "axios";
-import {
-  Button,
-  Form,
-} from "semantic-ui-react";
+import { Button, Form } from "semantic-ui-react";
 
 import { apiRoutes } from "../../constants";
 
 class ShareForm extends Component {
+  componentWillMount() {
+    let { fields } = this.props;
 
-  componentWillMount(){
+    if (fields) {
+      this.setState({ hasFields: true });
 
-      let { fields } = this.props;
+      fields.map((field, index) => {
+        this.inputState(field, index);
+      });
+    }
 
-      if (fields){
-
-          this.setState({hasFields: true});
-
-          fields.map((field, index) =>{
-              this.inputState(field, index);
-          })
-      }
-
-      this.setState({field2: 'TEST'})
+    this.setState({ field2: "TEST" });
   }
 
   // purely used to test mounted values for now
-  componentDidMount(){
-      console.log(this.state)
+  componentDidMount() {
+    console.log(this.state);
   }
 
-  submitForm = (formName) =>{
-
-    let { value0, value1  } = this.state;
+  submitForm = formName => {
+    let parent = this;
+    let { value0, value1 } = this.state;
 
     // TODO make a switch statement to handle both form cases
 
     // TODO consider refactoring this to a helper file
-    axios.post(apiRoutes.makeDonation, {
-      address: value0,
-      amount: value1
-    }, { headers: {"Access-Control-Allow-Origin": "*"}})
-      .then(function (response) {
-        console.log(response);
+    axios
+      .post(
+        apiRoutes.makeDonation,
+        {
+          address: value0,
+          amount: value1
+        },
+        { headers: { "Access-Control-Allow-Origin": "*" } }
+      )
+      .then(function(response) {
+        parent.setState({ donationID: response.data });
+        console.log(response.data);
       })
-      .catch(function (error) {
+      .catch(function(error) {
         console.log(error);
       });
+  };
 
-  }
-  handleChange = (e, { value }) => this.setState({ value });
+  inputState = (fieldObject, index) => {
+    Object.keys(fieldObject).map(key => {
+      // only creates state for the error / value variables
+      if (key === "error" || key === "value") {
+        // uses index argument to create scalable state for each object in this.fields
+        let stateVariable = `${key + index}`;
 
-  inputState = (fieldObject, index) =>{
-      Object.keys(fieldObject).map((key) =>{
-
-          // only creates state for the error / value variables
-          if (key === 'error' || key === 'value'){
-
-              // uses index argument to create scalable state for each object in this.fields
-              let stateVariable = `${key + index}`;
-
-              // sets the state key name and value
-              this.setState({ [stateVariable] : fieldObject[key]});
-          }
-
-      })
-  }
+        // sets the state key name and value
+        this.setState({ [stateVariable]: fieldObject[key] });
+      }
+    });
+  };
 
   inputChange = (value, fieldValue) => {
-    this.setState({[fieldValue]: value})
-  }
+    this.setState({ [fieldValue]: value });
+  };
 
-  renderFields = (fields) => {
-          return fields.map((field, index) =>{
+  renderFields = fields => {
+    return fields.map((field, index) => {
+      let { name, label, placeholder, value, error } = field;
 
-              let { name, label, placeholder, value, error} = field;
+      // creates state key names from index
+      let fieldValue = `${"value" + index}`;
 
-              // creates state key names from index
-              let fieldValue = `${'value' + index}`;
+      // expects a boolean
+      let fieldError = `${error + index}`;
 
-              // expects a boolean
-              let fieldError = `${error + index}`;
-
-              return(
-                  <Form.Input
-                      key={index}
-                      name={name}
-                      label={label}
-                      onChange={(e) =>{this.inputChange(e.target.value, fieldValue)}}
-                      placeholder={placeholder}
-                      error={this.state[fieldError]}
-                  />
-                  )
-
-          })
+      return (
+        <Form.Input
+          key={index}
+          name={name}
+          label={label}
+          onChange={e => {
+            this.inputChange(e.target.value, fieldValue);
+          }}
+          placeholder={placeholder}
+          error={this.state[fieldError]}
+        />
+      );
+    });
   };
 
   render() {
-    const { hasFields } = this.state;
+    const { hasFields, donationID } = this.state;
     let { fields } = this.props;
 
     // TODO add state handling for this.props.formName to handle makeDonation / fetchDonation form display
-    return (
-        <Fragment>
-            {hasFields ? <Form>
-                <Form.Group widths="equal">
-                    {this.renderFields(fields)}
-                </Form.Group>
-                <Form.Field onClick={() => this.submitForm()} control={Button}>Submit</Form.Field>
-            </Form> : <p>Form has no input props!</p>}
-      </Fragment>
 
+    // TODO fragment hasFields and donationID conditionals
+    return (
+      <Fragment>
+
+        {hasFields ? (
+          <Form>
+            <Form.Group widths="equal">{this.renderFields(fields)}</Form.Group>
+            <Form.Field onClick={() => this.submitForm()} control={Button}>
+              Submit
+            </Form.Field>
+          </Form>
+        ) : (
+          <p>Form has no input props!</p>
+        )}
+
+        {donationID ? (
+          <p>DonationID: {donationID}</p>
+        ) : (
+          <p>Submit the form to create your donation!</p>
+        )}
+
+      </Fragment>
     );
   }
 }
