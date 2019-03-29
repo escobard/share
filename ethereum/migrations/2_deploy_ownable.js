06bfafebee2e7dd8a81d7ccc06108db9ee57f180
Daniel Escobar <escobard90@gmail.com>
2019-03-28 18:27:39 -0600

    feat: split deployments into their own file, to create a transaction per contract deploy

---
+++ ethereum/migrations/2_deploy_ownable.js
@@ -0,0 +1,11 @@
+const Ownable = artifacts.require("./Ownable.sol");
+
+module.exports = function(deployer) {
+
+  // TODO - clean up logic here, can be vastly improved by following: https://medium.com/coinmonks/using-truffle-framework-in-an-advanced-way-7e32c11c97a9
+
+  // deploys initial Ownable contracts
+  deployer.deploy(Ownable, {gas: 7000000})
+
+
+};
---
+++ ethereum/migrations/4_deploy_lottery.js
@@ -0,0 +1,8 @@
+const LotteryRole = artifacts.require("./LotteryRole.sol"),
+  Ownable = artifacts.require("./Ownable.sol");
+
+module.exports = function(deployer) {
+  // deploys initial Ownable contracts
+  deployer.deploy(LotteryRole, Ownable.address, {gas: 7000000})
+
+};
