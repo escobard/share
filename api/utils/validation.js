b80d194d712dfb47bf00b60459266b1eea3c571d
Daniel Escobar <escobard90@gmail.com>
2019-04-14 17:47:12 -0600

    api-validation - feat: added Validation class, and exist skeleton

---
+++ api/utils/validation.js
@@ -0,0 +1,23 @@
+/** Utility to validate request values
+ * @dev add methods as necessary, keep as simple and re-usable as possible
+ */
+
+class Validation{
+  constructor(){
+    this.errors = [];
+  }
+
+  /** Checks if a value exists
+   * @param {*} value, property to validate
+   * @param {string} error, error added to the errors array
+   */
+
+  exists(value, error){
+
+  }
+
+
+}
+
+
+module.exports=Validation;
