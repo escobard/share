4e28d829ad84d423de0fa6952385733a49ffb58d
Daniel Escobar <escobard90@gmail.com>
2019-04-14 18:23:26 -0600

    api-validation - feat: middleware logic to validate / invalide null values complete

--- api/middlewares/makeDonationValidation.js
+++ api/middlewares/makeDonationValidation.js
@@ -1,8 +1,28 @@
+const Validation = require("../utils/validation");
+
 module.exports = async (req, res, next) => {
+  let {address_pu, address_pr, amount} = req.body;
+  let validation = new Validation();
+  let {exists, getErrors, clearErrors} = validation;
 
-  // null cases
+  // null case values validation
+  exists(address_pu, 'Public address must exist');
+  exists(address_pr, ' Private address must exist');
+  exists(amount, ' Amount must exist');
 
-  //
+  // rejects request in case of null values
+  let nullErrors = getErrors();
+
+  if (nullErrors.length >= 1){
+    res.status(400).json({
+      status: 'No null values allowed',
+      errors: nullErrors.join()
+    });
+  }
+
+  // existing value validation
+
+  // rejects request in case of invalid values
 
   next();
 };
