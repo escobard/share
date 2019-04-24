7716b3353d5ea30bedf215e30ef1eb7b114a648b
Daniel Escobar <escobard90@gmail.com>
2019-04-24 09:07:47 -0600

    api-validation - feat: added fetchDonationEtherValidation business validation logic, debugging time

--- api/middlewares/fetchDonationEtherValidation.js
+++ api/middlewares/fetchDonationEtherValidation.js
@@ -4,22 +4,51 @@ const Validation = require("../utils/validation");
  * @dev split apart from ether address validation, to fail fast if null
  */
 module.exports = async (req, res, next) => {
-  let {body: {address_pu, id }, web3, accounts: { owner_pu }, share } = req;
+  let {
+    body: { address_pu, id },
+    web3,
+    accounts: { owner_pu },
+    share
+  } = req;
   let validation = new Validation();
 
   let donationID = await share.methods.fetchDonationID.call({
     from: owner_pu
   });
 
-  await validation.isValidPublic(address_pu, web3, 'Public address is invalid');
-  await validation.customValidation(id >= donationID , ' Donation does not exist')
+  await validation.isValidPublic(address_pu, web3, "Public address is invalid");
+  await validation.customValidation(
+    id >= donationID,
+    " Donation does not exist"
+  );
 
   let etherErrors = validation.getErrors();
 
-  if (etherErrors.length >= 1){
+  if (etherErrors.length >= 1) {
     console.error("Ether validation errors:", etherErrors);
     return res.status(400).json({
-      status: 'Ether validation errors:',
+      status: "Ether validation errors:",
+      errors: etherErrors.join()
+    });
+  }
+
+  // handles ether business logic
+  let donation = await share.methods.fetchDonation(id).call({ from: owner_pu });
+
+  await validation.customValidation(
+    address_pu !==
+      (donation.donor.toLowerCase() ||
+        donation.owner.toLowerCase() ||
+        donation.charity.toLowerCase()),
+    " Public address must exist within fetched donation"
+  );
+
+  let etherBusinessErrors = validation.getErrors();
+
+  if (etherBusinessErrors.length >= 1) {
+    console.error("Ether business validation errors:", etherErrors);
+    return res.status(400).json({
+      status: "Ether business validation errors:",
       errors: etherErrors.join()
     });
   }
