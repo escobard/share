559bfedb94276c5d5fd6f6ee7e65bbafe1bae02e
Daniel Escobar <escobard90@gmail.com>
2019-04-27 16:36:00 -0600

    heroku-update-deployment - fix: fixed api process typo on health

--- api/index.js
+++ api/index.js
@@ -9,10 +9,12 @@ app.use(bodyParser.json());
 
 // TODO this needs to be refactored to a helper - needs to handle GCP logic after containerization
 if (process.env.NODE_ENV === "production"){
+  global.environment = "prod";
   app.use(cors({ origin: "https://share-ui.herokuapp.com" }));
 }
 else{
   app.use(cors({ origin: "http://localhost:3000" }));
+  global.environment = "dev";
 }
 
 require("./routes")(app);
