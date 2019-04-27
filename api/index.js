277b41caf0aff3e4dd1dfa887560a445a6024fbe
Daniel Escobar <escobard90@gmail.com>
2019-04-27 16:09:58 -0600

    heroku-update-deployment - feat: updated api cors origin for prod

--- api/index.js
+++ api/index.js
@@ -7,8 +7,13 @@ const express = require("express"),
 
 app.use(bodyParser.json());
 
-// TODO this needs to be updated for prod
-app.use(cors({ origin: "http://localhost:3000" }));
+// TODO this needs to be refactored to a helper - needs to handle GCP logic after containerization
+if (process.env.NODE_ENV === "production"){
+  app.use(cors({ origin: "https://share-ui.herokuapp.com/" }));
+}
+else{
+  app.use(cors({ origin: "http://localhost:3000" }));
+}
 
 require("./routes")(app);
 
